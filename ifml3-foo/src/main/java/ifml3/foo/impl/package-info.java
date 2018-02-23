1ac205af14e94e23f936461bd26c35a404c0aa42
Madzi <eliseevsan@gmail.com>
2018-02-24 00:10:10 +0100

    Remove demo modules

--- ifml3-foo/src/main/java/ifml3/foo/impl/package-info.java
+++
@@ -1,5 +0,0 @@
-
-@FileExtension(displayName = "%fooFileExtension.displayName", mimeType = FooUtils.MIME_TYPE, fileExtensions = FooUtils.FILE_EXTENSION, position = 10)
-package ifml3.foo.impl;
-
-import org.drombler.acp.core.data.FileExtension;
